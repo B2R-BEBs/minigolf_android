e9c1a88ced8eb80366a25da64887803b8f27ab7d
David Rihs <david.rihs@he-arc.ch>
2020-01-17 16:01:10 +0100

    Implemented resultactivity

--- app/src/main/java/ch/hearc/minigolf/data/api/Api.kt
+++ app/src/main/java/ch/hearc/minigolf/data/api/Api.kt
@@ -1,10 +1,10 @@
 package ch.hearc.minigolf.data.Api
 
 class Api private constructor(){
-
+    /*
     var RemoteDataSource = RemoteDataSource()
         private set
-
+*/
     companion object {
             @Volatile private var instance: Api? = null
 
