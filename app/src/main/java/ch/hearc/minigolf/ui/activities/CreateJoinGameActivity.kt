8ec1fa41ce90bdd1dd06d0f5bb2bc11ef1b67b25
David Rihs <david.rihs@he-arc.ch>
2020-01-19 00:18:46 +0100

    Add function joinGame in CreateJoinGameActivity

--- app/src/main/java/ch/hearc/minigolf/ui/activities/CreateJoinGameActivity.kt
+++ app/src/main/java/ch/hearc/minigolf/ui/activities/CreateJoinGameActivity.kt
@@ -3,11 +3,12 @@ package ch.hearc.minigolf.ui.activities
 import android.content.Intent
 import android.os.Bundle
 import android.text.InputFilter
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import ch.hearc.minigolf.R
+import ch.hearc.minigolf.data.models.Course
 import com.google.android.material.button.MaterialButton
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.textfield.TextInputEditText
 
 
@@ -17,26 +18,38 @@ class CreateJoinGameActivity : AppCompatActivity() {
         Intent(this, ChooseMinigolfActivity::class.java)
     }
 
+    private val intentGameInProgress: Intent by lazy {
+        Intent(this, GameInProgressActivity::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_join_game)
 
         val maxLength = resources.getInteger(R.integer.max_length)
 
-        val codeTextView = findViewById<TextInputEditText>(R.id.et_id_game)
+        val tokenEditText = findViewById<TextInputEditText>(R.id.et_token_game)
         val btnJoin = findViewById<MaterialButton>(R.id.mb_joinGame)
-        codeTextView.filters = arrayOf(InputFilter.AllCaps(), InputFilter.LengthFilter(maxLength))
+        tokenEditText.filters = arrayOf(InputFilter.AllCaps(), InputFilter.LengthFilter(maxLength))
 
-        codeTextView.addTextChangedListener {
-            btnJoin.isEnabled = codeTextView.length() == maxLength
+        tokenEditText.addTextChangedListener {
+            btnJoin.isEnabled = tokenEditText.length() == maxLength
         }
 
         btnJoin.setOnClickListener {
-
+            joinGame(tokenEditText.text.toString())
         }
 
         findViewById<MaterialButton>(R.id.mb_createGame).setOnClickListener {
-            startActivity( intentChooseMinigolf )
+            startActivity(intentChooseMinigolf)
         }
+    }
+
+    private fun joinGame(token: String) {
+        // TODO : get Course from API
+        Log.d("TOKEN", token)
+
+        //intentGameInProgress.putExtra(GameInProgressActivity.EXTRA_COURSE_OBJECT, course)
+        //startActivity(intentGameInProgress)
     }
 }
